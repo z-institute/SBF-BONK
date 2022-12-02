1031ea6de3bbb05b157ffd45bf8101c387c8ee5d
siling <think.sea@gmail.com>
2022-12-02 11:18:47 +0800

    modify hover css style

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,14 +1,7 @@
 import styles from "../styles/Home.module.css";
-import {
-  Box,
-  Stack,
-  Link,
-  Popover,
-  PopoverTrigger,
-  Spacer,
-} from "@chakra-ui/react";
+import { Box, Stack, Link, Spacer, scaleFadeConfig } from "@chakra-ui/react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
-import LocaleSwitcher from "./LocaleSwitcher"
+import LocaleSwitcher from "./LocaleSwitcher";
 
 const nav = () => {
   interface NavItem {
@@ -25,24 +18,26 @@ const nav = () => {
       label: "OPENSEA",
       href: "https://opensea.io/",
     },
-
   ];
 
   return (
     <Stack direction={"row"} spacing={4} className={styles["nav"]}>
       {NAV_ITEMS.map((navItem) => (
-        <Box key={navItem.label} p={5}>
+        <Box key={navItem.label} p={5} _hover={{ transform: "scale(1.1)" }}>
           <Link
             href={navItem.href ?? "#"}
             fontSize={"20px"}
             fontWeight={400}
             color="#ffffff"
+            _hover={{ textDecoration: "none" }}
           >
             {navItem.label}
           </Link>
         </Box>
       ))}
-      <LocaleSwitcher />
+      <Box _hover={{ transform: "scale(1.1)" }}>
+        <LocaleSwitcher />
+      </Box>
       <Spacer />
       <ConnectButton
         accountStatus="address"
