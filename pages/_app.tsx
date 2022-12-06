2153c9a094c5593c209e5ddeb9f6b7ef18a480b0
siling <think.sea@gmail.com>
2022-12-06 20:47:15 +0800

    add meta og tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -62,6 +62,33 @@ function MyApp({ Component, pageProps, router }: any) {
             <Head>
               <link rel="icon" type="image/jpg" href="/favicon.ico" />
               <meta
+                property="description"
+                content="在 FTX 交易所事件發生後，我們希望可以透過寓教於樂的方式傳遞數位資產的風險管理重要性，只要棒打 SBF 滿 111 下，就可以 free mint 一個 NFT，我們也延伸發起了一項社群串連送暖活動，讓品牌、商家及需要協助的用戶，找到所需的協助及優惠。"
+              />
+              <meta
+                property="og:url"
+                content="https://cyberbonk.zinstitute.net/"
+              />
+              <meta
+                property="og:image"
+                content="https://cyberbonk.zinstitute.net/_next/static/media/og-img.png"
+              ></meta>
+              <meta
+                property="og:title"
+                content="CyberBonk | 棒打 SBF 即可免費領取 NFT | #CareForFTX"
+              />
+              <meta name="twitter:card" content="summary" />
+              <meta name="twitter:title" content="CyberBonk" />
+              <meta
+                name="twitter:description"
+                content="在 FTX 交易所事件發生後，我們希望可以透過寓教於樂的方式傳遞數位資產的風險管理重要性，只要棒打 SBF 滿 111 下，就可以 free mint 一個 NFT，我們也延伸發起了一項社群串連送暖活動，讓品牌、商家及需要協助的用戶，找到所需的協助及優惠。"
+              />
+              <meta
+                name="twitter:image"
+                content="https://cyberbonk.zinstitute.net/_next/static/media/og-img.png"
+              />
+              {/* <title>CyberBonk | 棒打 SBF 即可免費領取 NFT | #CareForFTX</title> */}
+              <meta
                 name="google-site-verification"
                 content="G65CewReM3XHzCT__NPcpX9o6xAe27y-vNbivUq6ayQ"
               />
